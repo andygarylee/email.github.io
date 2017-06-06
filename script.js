716f07db1c54ad38274bc9b45cf2deb81e9ae76b
andygarylee <theandydude@gmail.com>
2017-06-05 22:28:45 -0400

    2nd commit
    need to figure out how to store values

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-function submitInput(){
-    var emailsInput = 0;
-    alert('test worked');
+var emailSubmission =[];
 
+function submitInput(){
+    var newEmail = document.getElementById('emailSub').value;
+    emailSubmission.push(newEmail);
+    localStorage.setItem("emailSubmission", emailSubmission);
+    document.getElementById("confirmation").innerHTML = "submitted";
+
+    }
+
+function timeout(){
+setTimeout(timeout,50000);
+window.location.reload(true);
+}
+
+function accessList(){
+    var txt;
+    var password = prompt("Enter the password", );
+    if(password == "toast" ){
+    txt = "here is the list";
+    //document.getElementById("list").innerHTML = txt;
+    document.getElementById("list").innerHTML = emailSubmission;
+    //show emails variable here
+    }
+    else{
+        txt = "password incorrect";
+        document.getElementById("list").innerHTML = txt;
+    }
+ //also add emails variable here
 }
